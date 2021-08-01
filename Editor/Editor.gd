c681388635b97bc48c078b24cf8c97fcad7a00d4
skyfire222 <skyfire222@users.noreply.github.com>
2021-08-01 16:36:54 -0400

    Start on editor

--- Editor/Editor.gd
+++ Editor/Editor.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ViewportContainer/Viewport/Camera2D.make_current()
+#	$ViewportContainer/Viewport/Camera2D.make_current()
 	update()
 
 
